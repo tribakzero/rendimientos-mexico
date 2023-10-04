5b4fa63d8508a12308ffb17456ad78a141bb67a1
Alejandro Camarillo <tribak.zero@gmail.com>
2023-10-03 20:38:17 -0600

    Feat: Updates footer to add repo link

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className=" flex py-4 border-t border-gray-200 justify-center text-center">
-      <a href="https://github.com/tribakzero">
-        Con todo el cocoro, por tribak
+      <a href="https://github.com/tribakzero/rendimientos-mexico">
+        Contribuye al proyecto en Github
       </a>
     </footer>
   )
